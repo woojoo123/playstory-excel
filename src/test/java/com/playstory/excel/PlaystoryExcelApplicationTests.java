7adfd49519856562b20c442fed99c5793cc58f11
woojoo123 <woozoo0926@naver.com>
2026-05-31 10:37:21 +0900

    스프링 부트 프로젝트 초기 구성

---
+++ src/test/java/com/playstory/excel/PlaystoryExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playstory.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystoryExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
